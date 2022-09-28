b012056d7b080773cddd5175bc75b5021071a2b7
ConstantineTr <114101642+ConstantineTr@users.noreply.github.com>
2022-09-28 20:23:38 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dortislem dortIslem = new Dortislem();
+        int sonuc = dortIslem.Carp(3, 4);
+        System.out.println(sonuc);
+    }
+}
